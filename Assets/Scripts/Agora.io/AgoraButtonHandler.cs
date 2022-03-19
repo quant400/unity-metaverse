8806336183795fa976394df2d6812ecdae23de58
JhonataBx <jhonatabxd@gmail.com>
2022-03-19 03:42:33 -0300

    Configurando Agora.io nas cenas
    Signed-off-by: JhonataBx <jhonatabxd@gmail.com>

---
+++ Assets/Scripts/Agora.io/AgoraButtonHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AgoraButtonHandler : MonoBehaviour
+{
+
+
+    private AgoraHome agoraHome;
+
+    public void Awake()
+    {
+        FindManager();
+    }
+
+
+    private void FindManager()
+    {
+        if (agoraHome != null)
+        {
+            AgoraHome gameController = GameObject.Find("AgoraManager").GetComponent<AgoraHome>();
+            if (gameController == null)
+            {
+                Debug.LogError("Missing game controller...");
+                return;
+            }
+        }
+    }
+
+
+    public void Leave()
+    {
+        agoraHome.onLeaveButtonClicked();
+    }
+}
